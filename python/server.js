ab21c459533c02e003a53638da607cb3aa0aa305
peraltaf <63076583+peraltaf@users.noreply.github.com>
2024-01-03 13:55:23 -0800

    Create server.js

---
+++ python/server.js
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from configparser import ConfigParser
+from json import dumps
+import api
+
+
+app = Flask(__name__)
+
+env = 'dev'
+config = ConfigParser(interpolation=None)
+config.read('config.ini')
+BASE_URL = config.get(env, 'url')
+USER = config.get(env, 'user')
+NAMESPACE = config.get(env, 'namespace')
+PASSWORD = config.get(env, 'password')
+
+ca = api.CognosAPI(BASE_URL, ssl_verify=False)
+ca.login(USER, PASSWORD, NAMESPACE)
+
+@app.route('/get_reports')
+def get_reports():
+    folder_id = request.args.get('folder_id')
+
+    if folder_id is None or len(folder_id) == 0:
+        return 'You must include a folder_id parameter'
+    
+    contents = ca.get_reports(folder_id)
+    return jsonify(contents)
+
+@app.route('/get_permissions')
+def get_permissions():
+    object_id = request.args.get('object_id')
+
+    if object_id is None or len(object_id) == 0:
+        return 'You must include a object_id parameter'
+    
+    permissions = ca.get_permissions(object_id)
+    return jsonify(permissions)
+
+if __name__ == '__main__':  
+   app.run()
